83c56126809ebb8a836aac85ee2584889a422c03
Rafli andreansyah <47078618+rafliandreansyah@users.noreply.github.com>
2024-06-27 23:48:24 +0700

    feat: finishing export csv

--- src/main/java/com/example/reporting_test/controller/ReportingController.java
+++ src/main/java/com/example/reporting_test/controller/ReportingController.java
@@ -1,10 +1,7 @@
 package com.example.reporting_test.controller;
 
 import java.io.IOException;
-import java.io.StringWriter;
-import java.text.NumberFormat;
 import java.time.LocalDateTime;
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.http.HttpHeaders;
@@ -17,10 +14,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import com.example.reporting_test.model.DataDummy;
 import com.example.reporting_test.model.GenerateNoteTransactionData;
 import com.example.reporting_test.model.TransactionData;
+import com.example.reporting_test.service.CsvUtilService;
 import com.example.reporting_test.service.MarkdownUtilService;
 import com.example.reporting_test.service.PdfUtilService;
 import com.lowagie.text.DocumentException;
-import com.opencsv.CSVWriter;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.servlet.http.HttpServletResponse;
@@ -33,6 +30,7 @@ public class ReportingController {
 
     final PdfUtilService pdfUtilService;
     final MarkdownUtilService markdownUtilService;
+    final CsvUtilService csvUtilService;
     List<TransactionData> data;
 
     @PostConstruct
@@ -61,10 +59,6 @@ public class ReportingController {
         }).reduce(0.0, (a, b) -> a + b);
 
         var potongan = 0.0;
-        var total = subTotal - potongan;
-
-        NumberFormat formatter = NumberFormat.getCurrencyInstance();
-        //markdownUtilService.generateMarkdown(data);
 
         model.addAttribute("generateNoteTransactionData", GenerateNoteTransactionData.builder()
         .seller(dummySeller)
@@ -146,25 +140,61 @@ public class ReportingController {
 
     @GetMapping("/exports/csv")
     public void generateCsv(HttpServletResponse response) throws IOException {
-        // Sample data
-        List<String[]> data = Arrays.asList(
-                new String[]{"ID", "Name", "Age"},
-                new String[]{"1", "Alice", "30"},
-                new String[]{"2", "Bob", "25"}
-        );
+        // general info
+        final String dummySeller = "Roben Diaz";
+        final String dummyStore = "PS.SWALAYAN & DEPT.STORE";
+        final LocalDateTime dateCreated = LocalDateTime.now();
+        final Integer dummyPage = 1;
 
-        // Create CSV string using OpenCSV
-        StringBuilder csvString = new StringBuilder();
-        try (StringWriter writer = new StringWriter(); CSVWriter csvWriter = new CSVWriter(writer)) {
-            csvWriter.writeAll(data);
-            csvString.append(writer.toString());
-        }
+        // nota info
+        final LocalDateTime dateNote = LocalDateTime.of(2022, 3, 1, 00, 00);
+        final String numberNote = "001-727254";
+        final String customerNote = "SI254-H.ACEP";
+        final String address = "jl. panglima sudirman no.21";
+        final String customerName = "H.ACEP";
+        final String fileName = "Invoice_" + LocalDateTime.now().getNano();
+
+        var subTotal = data.stream().map((tr) -> {
+            return tr.getTotalPrice();
+        }).reduce(0.0, (a, b) -> a + b);
+
+        var potongan = 0.0;
+        // // Sample data
+        // List<String[]> data = Arrays.asList(
+        //         new String[]{"ID", "Name", "Age"},
+        //         new String[]{"1", "Alice", "30"},
+        //         new String[]{"2", "Bob", "25"}
+        // );
+
+        // // Create CSV string using OpenCSV
+        // StringBuilder csvString = new StringBuilder();
+        // try (StringWriter writer = new StringWriter(); CSVWriter csvWriter = new CSVWriter(writer)) {
+        //     csvWriter.writeAll(data);
+        //     csvString.append(writer.toString());
+        // }
+
+        var csvString = csvUtilService.generateCsv(GenerateNoteTransactionData.builder()
+        .seller(dummySeller)
+        .store(dummyStore)
+        .pageNote(dummyPage)
+        .dateCreatedNote(dateCreated)
+        .timeCreatedNote(dateCreated)
+        .dateTransactionNote(dateNote)
+        .transactionIdNote(numberNote)
+        .customerNameNote(customerNote)
+        .customerAddressNote(address)
+        .transactions(data)
+        .customerName(customerName)
+        .subtotal(subTotal)
+        .potongan(potongan)
+        .build()
+        );
 
         // Configure response for CSV download
         response.setContentType("text/csv");
-        response.setHeader("Content-Disposition", "attachment; filename=data.csv");
+        response.setHeader("Content-Disposition", "attachment; filename=" + fileName + ".csv");
 
         // Write CSV string to response
-        response.getWriter().write(csvString.toString());
+        response.getWriter().write(csvString);
     }
 }
---
+++ src/main/java/com/example/reporting_test/service/CsvUtilService.java
@@ -0,0 +1,92 @@
+package com.example.reporting_test.service;
+
+import java.io.IOException;
+import java.io.StringWriter;
+
+import org.springframework.stereotype.Service;
+
+import com.example.reporting_test.model.GenerateNoteTransactionData;
+import com.example.reporting_test.model.TransactionData;
+import com.opencsv.CSVWriter;
+
+@Service
+public class CsvUtilService {
+
+    public String generateCsv(GenerateNoteTransactionData transactionData) throws IOException 
+    {
+        // Info general
+        String[] row1Note = {transactionData.getSeller(), " ", " ", " ", "TANGGAL", transactionData.getDateCreatedNoteFormat()};
+        String[] row2Note = {transactionData.getStore(), " ", " ", " ", "JAM",transactionData.getTimeCreatedNoteFormat()};
+        String[] row3Note = {" ", " ", " ", " ", "TANGGAL", transactionData.getDateCreatedNoteFormat()};
+        
+        // Space
+        String[] spaceNote = {"", " ", " ", " ", " ", ""};
+
+        // Info Note
+        String[] row5Note = {" ", " ", "NOTA PENJUALAN", " ", " ", ""};
+        String[] row6Note = {" ", " ", "TANGGAL", transactionData.getDateTransactionNoteFormat(), " ", ""};
+        String[] row7Note = {" ", " ", "NOMOR", transactionData.getTransactionIdNote(), " ", ""};
+        String[] row8Note = {" ", " ", "PELANGGAN", transactionData.getCustomerNameNote(), " ", ""};
+        String[] row9Note = {" ", " ", "ALAMAT", transactionData.getCustomerAddressNote(), " ", ""};
+
+        // Table HEADER
+        String[] headerCSV = {"NO.", "NO.PLU", "NAMA", "QTY", "H.STN", "NETTO"};
+
+
+        // Total
+        String[] subTotalRow = {" ", " ", " ", " ", "SUBTOTAL", transactionData.getSubTotalFormat()};
+        String[] potonganRow = {" ", " ", " ", " ", "POTONGAN", transactionData.getPotonganFormat()};
+        String[] totalRow = {" ", " ", " ", " ", "TOTAL", transactionData.getTotalFormat()};
+
+        // Signature
+        String[] row1Signature = {" ", "PELANGGAN", " ", " ", "HORMAT KAMI", " "};
+        String[] row2Signature = {" ", "( " + transactionData.getCustomerName() +" )", " ", " ", "(       )", " "};
+
+        // Generate to string
+        StringBuilder csvString = new StringBuilder();
+        try (StringWriter sw = new StringWriter(); CSVWriter csvWriter = new CSVWriter(sw)) {
+            // General Info
+            csvWriter.writeNext(row1Note);
+            csvWriter.writeNext(row2Note);
+            csvWriter.writeNext(row3Note);
+            csvWriter.writeNext(spaceNote);
+
+            // Info Note
+            csvWriter.writeNext(row5Note);
+            csvWriter.writeNext(row6Note);
+            csvWriter.writeNext(row7Note);
+            csvWriter.writeNext(row8Note);
+            csvWriter.writeNext(row9Note);
+
+            //Header TABLE CSV
+            csvWriter.writeNext(headerCSV);
+            // Looping data list table
+            var index = 1;
+            for (TransactionData data : transactionData.getTransactions() ){
+                String[] dataItem = {index + ".", data.getNoPlu(), data.getProductName(), data.getQuantity().toString(), data.getPrice().toString(), data.getTotalPrice().toString()};
+                csvWriter.writeNext(dataItem);
+
+                index += 1;
+            }
+
+
+            // Total
+            csvWriter.writeNext(subTotalRow);
+            csvWriter.writeNext(potonganRow);
+            csvWriter.writeNext(totalRow);
+            csvWriter.writeNext(spaceNote);
+            csvWriter.writeNext(spaceNote);
+
+            // Signature
+            csvWriter.writeNext(row1Signature);
+            csvWriter.writeNext(spaceNote);
+            csvWriter.writeNext(row2Signature);
+
+            // Append all to stringbuilder
+            csvString.append(sw.toString());
+        }
+
+        return csvString.toString();
+    }
+
+}
